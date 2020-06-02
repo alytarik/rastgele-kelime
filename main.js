33869627bebf890e5063d5bfd2084b43ea77c8ce
neden00 <alimelik55@gmail.com>
2020-06-02 15:19:42 +0300

    Optimized randomise function.

--- main.js
+++ main.js
@@ -1,42 +1,20 @@
-function readJSON(path) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', path, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function(e) { 
-      if (this.status == 200) {
-          var file = new File([this.response], 'temp');
-          var fileReader = new FileReader();
-          fileReader.addEventListener('load', function(){
-               //do stuff with fileReader.result
-               var mydata=JSON.parse(fileReader.result);
+randomise = function () {
+    var lis = document.getElementById("container").getElementsByTagName('li');
+    var matchingLetters = document.getElementById("matchingLetters").value;
+    var mastarEki = document.getElementById("mastarEki").checked;
 
+    var fWord = words[Math.floor(Math.random() * words.length)];
+    while(!mastarEki && (fWord.endsWith("mek")||fWord.endsWith("mak")||fWord.endsWith("me")||fWord.endsWith("ma")))
+        fWord = words[Math.floor(Math.random() * words.length)];
+    var validWords = words.filter(word => word.endsWith(fWord.slice(-matchingLetters)));
+    if(!mastarEki)
+        validWords=validWords.filter(word => !(word.endsWith("mek")||word.endsWith("mak")||word.endsWith("me")||word.endsWith("ma")))
 
-               var lis=document.getElementById("container").getElementsByTagName('li');
+    if(validWords.length<4) return;
 
-               var fWord = mydata[(Math.floor(Math.random()*Object.keys(mydata).length)).toString()];
-               lis[0].innerHTML=fWord;
-                console.log(fWord);
+    lis[0].innerHTML = fWord;
 
-               var words=[];
-               for (let i = 0; i < 5; i++) {
-                    var word = mydata[(Math.floor(Math.random()*Object.keys(mydata).length)).toString()];
-                    while(word.slice(-2) != fWord.slice(-2)) {
-                        word = mydata[(Math.floor(Math.random()*Object.keys(mydata).length)).toString()];
-                    }
-                    words.push(word);
-                }
-                    
-                for(let i =1;i<5;i++){
-                    lis[i].innerHTML=words[i];
-                }
-               
-          });
-          fileReader.readAsText(file);
-      } 
-    }
-    xhr.send();
-}
-
-randomise = function() {
-    readJSON("data.json");
+    validWords.sort( function() { return 0.5 - Math.random() } );
+    for (let i = 1; i < 5; i++)
+        lis[i].innerHTML = validWords[i];
 };
